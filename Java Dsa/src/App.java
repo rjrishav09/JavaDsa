447f5447edaae5d4e32f760b6d039604538a10a0
Lenovo <crazyrishavhacker@gmail.com>
2023-11-02 22:24:57 +0530

    Second commit

--- Java Dsa/src/App.java
+++ Java Dsa/src/App.java
@@ -1,5 +1,10 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        for(int i=0;i<10;i++){
+            for(int j=0;j<10;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println(" ");
+        }
     }
 }
