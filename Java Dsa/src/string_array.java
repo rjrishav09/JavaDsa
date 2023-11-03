8116ab45782645d39d51b279957ffc7c84a14e32
Lenovo <crazyrishavhacker@gmail.com>
2023-11-03 18:33:45 +0530

    string array

---
+++ Java Dsa/src/string_array.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class string_array {
+
+    public static void main(String[] args) {
+        String data[] = new String[5];
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the name of students :");
+        for(int i =0;i<data.length;i++){
+            data[i] =scan.nextLine();
+        }
+        for(int i=0; i<=data.length;i++){
+            System.out.println(i+1 + " : " + data[i]);
+        }
+    
+
+    }
+    
+}
