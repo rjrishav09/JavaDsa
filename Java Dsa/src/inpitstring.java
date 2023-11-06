85043cab5a6737bf4523a8f0aee7468b275a84d6
Lenovo <crazyrishavhacker@gmail.com>
2023-11-06 11:30:15 +0530

    9

--- Java Dsa/src/inpitstring.java
+++ Java Dsa/src/inpitstring.java
@@ -4,9 +4,15 @@ import java.util.Scanner;
 public class inpitstring {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String name[] = new String[2];
         System.out.println("Enter your name: ");
-        String name = scan.nextLine();
-        System.out.println("Your name is: " + name);
+        for(int i =0;i<name.length;i++){
+            name[i] = scan.nextLine();
+        }
+        for(int i=0;i<name.length;i++){
+
+            System.out.println("Your name is: " + name[i]);
+        }
     }
     
 }
