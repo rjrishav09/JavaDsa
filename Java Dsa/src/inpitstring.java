99b387fbdf9a3685b04c8077d0bfbb12120912ee
Lenovo <crazyrishavhacker@gmail.com>
2023-11-06 11:23:39 +0530

    8

---
+++ Java Dsa/src/inpitstring.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class inpitstring {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scan.nextLine();
+        System.out.println("Your name is: " + name);
+    }
+    
+}
