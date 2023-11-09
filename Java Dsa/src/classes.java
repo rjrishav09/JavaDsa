4b8a5523ef598fd245a3c6891efbbcb1e9ccb8fa
Lenovo <crazyrishavhacker@gmail.com>
2023-11-09 09:22:00 +0530

    6

--- Java Dsa/src/classes.java
+++ Java Dsa/src/classes.java
@@ -1,12 +1,28 @@
+import java.util.Scanner;
+
 class Display {
     public static void display() {
         System.out.println("Welcome");
     }
 }
 
+class Inputs{
+    public static int inputs(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your numbers: ");
+        int a = scan.nextInt();
+        return a;
+        
+    }
+}
+
 public class classes {
     public static void main(String[] args) {
         Display d = new Display();
+        Inputs k = new Inputs();
+        k.inputs();
         d.display(); // Call the static method from the Display class
     }
 }
+
+
