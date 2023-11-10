9762e49f9f8dd2d9f421bbf0e8d29addf09eb4b5
Lenovo <crazyrishavhacker@gmail.com>
2023-11-10 07:28:55 +0530

    class

--- Java Dsa/src/classes.java
+++ Java Dsa/src/classes.java
@@ -15,13 +15,26 @@ class Inputs{
         
     }
 }
+class Table{
+    public static void table(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter YOur number");
+        int t = scan.nextInt();
+        for(int i =1;i<11;++i){
+            System.out.println(t + " * " + i + " = " + i*t);
+        }
+
+    }
+}
 
 public class classes {
     public static void main(String[] args) {
         Display d = new Display();
         Inputs k = new Inputs();
+        Table p = new Table();
         k.inputs();
-        d.display(); // Call the static method from the Display class
+        d.display(); 
+        p.table();
     }
 }
 
